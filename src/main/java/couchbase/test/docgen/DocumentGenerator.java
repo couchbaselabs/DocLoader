7a3a9227a391479249e187d4b5f88f7c175b0848
riteshagarwalcouchbase <ritesh.agarwal@couchbase.com>
2023-11-02 14:45:43 -0700

    Fix: Adding nimbus type workload to the docloader.

--- src/main/java/couchbase/test/docgen/DocumentGenerator.java
+++ src/main/java/couchbase/test/docgen/DocumentGenerator.java
@@ -6,16 +6,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import couchbase.test.key.CircularKey;
 import couchbase.test.key.RandomKey;
 import couchbase.test.key.RandomSizeKey;
 import couchbase.test.key.ReverseKey;
 import couchbase.test.key.SimpleKey;
-import reactor.util.function.Tuple2;
-import reactor.util.function.Tuples;
 import couchbase.test.val.Hotel;
+import couchbase.test.val.MongoHotel;
+import couchbase.test.val.NimbusM;
+import couchbase.test.val.NimbusP;
 import couchbase.test.val.SimpleValue;
 import couchbase.test.val.anySizeValue;
-import couchbase.test.docgen.KVGenerator;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
 abstract class KVGenerator{
     public WorkLoadSettings ws;
@@ -33,19 +36,28 @@ abstract class KVGenerator{
         this.ws = ws;
         if(keyClass.equals(RandomKey.class.getSimpleName()))
             this.keyInstance = RandomKey.class;
-        else if(keyClass == ReverseKey.class.getSimpleName())
+        else if(keyClass.equals(ReverseKey.class.getSimpleName()))
             this.keyInstance = ReverseKey.class;
-        else if(keyClass == RandomSizeKey.class.getSimpleName())
+        else if(keyClass.equals(RandomSizeKey.class.getSimpleName()))
             this.keyInstance = RandomSizeKey.class;
+        else if(keyClass.equals(CircularKey.class.getSimpleName()))
+            this.keyInstance = CircularKey.class;
         else
             this.keyInstance = SimpleKey.class;
 
-        if(valClass.equals(anySizeValue.class.getSimpleName()))
+        if(valClass.equals(MongoHotel.class.getSimpleName()))
+            this.valInstance = MongoHotel.class;
+        else if(valClass.equals(anySizeValue.class.getSimpleName()))
             this.valInstance = anySizeValue.class;
+        else if (valClass.equals(NimbusP.class.getSimpleName()))
+            this.valInstance = NimbusP.class;
+        else if (valClass.equals(NimbusM.class.getSimpleName()))
+            this.valInstance = NimbusM.class;
         else if (valClass.equals(Hotel.class.getSimpleName()))
-        	this.valInstance = Hotel.class;
+            this.valInstance = Hotel.class;
         else
             this.valInstance = SimpleValue.class;
+
         try {
             this.keys = keyInstance.getConstructor(WorkLoadSettings.class).newInstance(ws);
             this.vals = valInstance.getConstructor(WorkLoadSettings.class).newInstance(ws);
@@ -66,30 +78,60 @@ abstract class KVGenerator{
         }
     }
 
-    public boolean has_next() {
-        return this.ws.dr.createItr.get() < this.ws.dr.create_e;
+    public boolean has_next_create() {
+        if (this.ws.dr.createItr.get() < this.ws.dr.create_e)
+            return true;
+        return false;
     }
 
     public boolean has_next_read() {
-        return this.ws.dr.readItr.get() < this.ws.dr.read_e;
+        if (this.ws.dr.readItr.get() < this.ws.dr.read_e)
+            return true;
+        if (this.keyInstance.getSimpleName() == CircularKey.class.getSimpleName()) {
+            this.resetRead();
+            return true;
+        }
+        return false;
     }
 
     public boolean has_next_update() {
-        return this.ws.dr.updateItr.get() < this.ws.dr.update_e;
+        if (this.ws.dr.updateItr.get() < this.ws.dr.update_e)
+            return true;
+        if (this.keyInstance.getSimpleName() == CircularKey.class.getSimpleName()) {
+            this.resetUpdate();
+            return true;
+        }
+        return false;
     }
 
     public boolean has_next_expiry() {
-        return this.ws.dr.expiryItr.get() < this.ws.dr.expiry_e;
+        if (this.ws.dr.expiryItr.get() < this.ws.dr.expiry_e)
+            return true;
+        if (this.keyInstance.getSimpleName() == CircularKey.class.getSimpleName()) {
+            this.resetExpiry();
+            return true;
+        }
+        return false;
     }
 
     public boolean has_next_delete() {
-        return this.ws.dr.deleteItr.get() < this.ws.dr.delete_e;
+        if (this.ws.dr.deleteItr.get() < this.ws.dr.delete_e)
+            return true;
+        return false;
     }
 
     abstract Tuple2<String, Object> next();
 
     void resetRead() {
         this.ws.dr.readItr =  new AtomicLong(this.ws.dr.read_s);
+    }
+
+    void resetExpiry() {
+        this.ws.dr.expiryItr.set(this.ws.dr.expiry_s);
+    }
+
+    void resetUpdate() {
+        this.ws.dr.updateItr.set(this.ws.dr.update_s);
     }
 }
 
@@ -98,6 +140,10 @@ public class DocumentGenerator extends KVGenerator{
 
     public DocumentGenerator(WorkLoadSettings ws, String keyClass, String valClass) throws ClassNotFoundException {
         super(ws, keyClass, valClass);
+    }
+
+    public WorkLoadSettings get_work_load_settings() {
+        return this.ws;
     }
 
     public Tuple2<String, Object> next() {
@@ -155,7 +201,7 @@ public class DocumentGenerator extends KVGenerator{
     public List<Tuple2<String, Object>> nextInsertBatch() {
         List<Tuple2<String, Object>> docs = new ArrayList<Tuple2<String,Object>>();
         int count = 0;
-        while (this.has_next() && (count<ws.batchSize*ws.creates/100)) {
+        while (this.has_next_create() && count<ws.batchSize*ws.creates/100) {
             docs.add(this.next());
             count += 1;
         }
@@ -207,6 +253,4 @@ public class DocumentGenerator extends KVGenerator{
         }
         return docs;
     }
-
 }
-
